bd286bc987bc3fea95554f09ad21dea1f4c5d3b5
klchen0112 <32459567+klchen0112@users.noreply.github.com>
2024-07-27 17:54:53 +0000

    add dae packages

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -45,14 +45,16 @@ fi
 #科学插件设置
 if [[ $WRT_REPO == *"lede"* ]]; then
 	echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-passwall=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
+	# echo "CONFIG_PACKAGE_luci-app-passwall=y" >> ./.config
+	# echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-turboacc=y" >> ./.config
 else
 	echo "CONFIG_PACKAGE_luci=y" >> ./.config
 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
 	echo "CONFIG_PACKAGE_dae=y" >> ./.config
+	echo "CONFIG_PACKAGE_dae-geoip=y" >> ./.config
+	echo "CONFIG_PACKAGE_dae-geosite=y" >> ./.config
 	echo "CONFIG_PACKAGE_daed=y" >> ./.config
 	# echo "CONFIG_PACKAGE_luci-app-daed=y" >> ./.config
 fi
