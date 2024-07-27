2a161cd47ab8d43c36a7b7e3b2efe5e07a778d9e
klchen0112 <32459567+klchen0112@users.noreply.github.com>
2024-07-27 13:44:06 +0000

    change to dead

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -26,13 +26,13 @@ UPDATE_PACKAGE "design" "0x676e67/luci-theme-design" "$([[ $WRT_REPO == *"lede"*
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "$([[ $WRT_REPO == *"lede"* ]] && echo "main" || echo "js")"
 
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
-UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
+# UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
+# UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
-UPDATE_PACKAGE "advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
+# UPDATE_PACKAGE "advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
 UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
 
-UPDATE_PACKAGE "dae" "QiuSimons/luci-app-daed" "master"
+UPDATE_PACKAGE "daed" "QiuSimons/luci-app-daed" "master"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
 if [[ $WRT_REPO != *"lede"* ]]; then
