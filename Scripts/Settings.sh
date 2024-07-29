68b6f2f8d1de481b2e9fcd2aa316feb41476ca77
klchen0112 <32459567+klchen0112@users.noreply.github.com>
2024-07-29 06:30:54 +0000

    remove settings

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -54,22 +54,6 @@ else
 	echo "CONFIG_PACKAGE_luci=y" >> ./.config
 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
-	echo "define KernelPackage/xdp-sockets-diag
-		SUBMENU:=$(NETWORK_SUPPORT_MENU)
-		TITLE:=PF_XDP sockets monitoring interface support for ss utility
-		KCONFIG:= \
-			CONFIG_XDP_SOCKETS=y \
-			CONFIG_XDP_SOCKETS_DIAG
-		FILES:=$(LINUX_DIR)/net/xdp/xsk_diag.ko
-		AUTOLOAD:=$(call AutoLoad,31,xsk_diag)
-		endef
-
-		define KernelPackage/xdp-sockets-diag/description
-		Support for PF_XDP sockets monitoring interface used by the ss tool
-		endef
-
-		$(eval $(call KernelPackage,xdp-sockets-diag))
-	" >> ./package/kernel/linux/modules/netsupport.mk
 	echo "CONFIG_PACKAGE_dae=y" >> ./.config
 	echo "CONFIG_PACKAGE_dae-geoip=y" >> ./.config
 	echo "CONFIG_PACKAGE_dae-geosite=y" >> ./.config
