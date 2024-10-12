e4603820e11984b44bdc573b31037766137fdd20
VIKING <viking.yfy@qq.com>
2024-10-12 20:15:44 +0800

    项目调整

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -43,6 +43,7 @@ UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "luci-app-netspeedtest" "muink/luci-app-netspeedtest" "master"
 UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/luci-app-wolplus" "main"
 if [[ $WRT_REPO == *"openwrt-6.x"* ]]; then
+if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
 fi
 
