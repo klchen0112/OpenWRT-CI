5861197da0bfc14fe1ef6688aaf524b47a649071
klchen0112 <klchen0112@gmail.com>
2024-12-21 20:54:03 +0800

    use sing-box

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -51,10 +51,12 @@ fi
 if [[ $WRT_TARGET != *"MT7621"* ]]; then
     echo "CONFIG_PACKAGE_luci-app-netspeedtest=n" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-natmap=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+	echo "CONFIG_PACKAGE_sing-box=y" >> ./.config #DNS服务器
+	echo "CONFIG_PACKAGE_luci-app-homeproxy=n" >> ./.config
 	echo "CONFIG_PACKAGE_dae=n" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-daed=n" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> ./.config #DNS服务器
+	echo "CONFIG_PACKAGE_mosdns=y" >> ./.config #DNS服务器
+	echo "CONFIG_PACKAGE_luci-app-mosdns=n" >> ./.config #DNS服务器
 	echo "CONFIG_PACKAGE_luci-app-uugamebooster=n" >> ./.config # uu游戏
 	echo "CONFIG_PACKAGE_tailscale=y" >> ./.config #vpn
 	echo "CONFIG_PACKAGE_tailscaled=y" >> ./.config #vpn
