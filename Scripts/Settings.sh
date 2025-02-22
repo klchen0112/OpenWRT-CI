35f53a5153da4299f25dbdc84653a278fb2fd9eb
klchen0112 <klchen0112@gmail.com>
2025-02-22 17:13:15 +0800

    update dae

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -52,9 +52,9 @@ if [[ $WRT_TARGET != *"MT7621"* ]]; then
     echo "CONFIG_PACKAGE_luci-app-netspeedtest=n" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-natmap=y" >> ./.config
 	echo "CONFIG_PACKAGE_sing-box=y" >> ./.config #DNS服务器
-	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-homeproxy=n" >> ./.config
 	echo "CONFIG_PACKAGE_mosdns=y" >> ./.config #DNS服务器
-	echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> ./.config #DNS服务器
+	echo "CONFIG_PACKAGE_luci-app-mosdns=n" >> ./.config #DNS服务器
 	echo "CONFIG_PACKAGE_luci-app-uugamebooster=n" >> ./.config # uu游戏
 	echo "CONFIG_PACKAGE_tailscale=y" >> ./.config #vpn
 	echo "CONFIG_PACKAGE_tailscaled=y" >> ./.config #vpn
@@ -80,7 +80,7 @@ if [[ $WRT_TARGET != *"MT7621"* && $WRT_TARGET != *"IPQ"* ]]; then
 	echo "CONFIG_USE_GC_SECTIONS=y" >> ./.config
 	echo "CONFIG_USE_LTO=y" >> ./.config
 	echo "CONFIG_PACKAGE_dae=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-daed=y" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-daed=n" >> ./.config
 
 fi
 
